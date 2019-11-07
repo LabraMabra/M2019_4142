a83349aab5a93e9fc5474d333e88629d76591b21
Baykov-Fedor <55537141+Baykov-Fedor@users.noreply.github.com>
2019-11-07 13:21:46 +0300

    Update task_04.R

--- Baykov_Fedor/task_04.R
+++ Baykov_Fedor/task_04.R
@@ -1,5 +1,6 @@
 weather1 <- select (weather, -X)
-weather2 <- gather(weather1, key = day, value = val, X1 : X31)
+names(weather1)[4:34] <- '1':'31'
+weather2 <- gather(weather1, key = day, value = val, '1' : '31')
 weather3 <- unite(weather2, col = y_m_d, year, month, day)
 weather4 <- spread(weather3, key = measure, value = val)
 weather5 <- weather4[order(weather4$y_m_d),]
