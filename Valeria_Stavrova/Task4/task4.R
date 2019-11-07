762c7b2d72ca6cd80f8ee5a8dc090da4aefd631f
ValeriaStavrova <55492085+ValeriaStavrova@users.noreply.github.com>
2019-11-07 17:17:24 +0300

    Update task4.R

--- Valeria_Stavrova/Task4/task4.R
+++ Valeria_Stavrova/Task4/task4.R
@@ -14,11 +14,9 @@ View(weather)
 library(tidyr)
 newweather <-weather[,-1] %>% gather(key = day, value = val, X1 : X31)  %>% 
   pivot_wider(names_from = measure, values_from = val)  
-
 View(newweather)
 
 #PREPARING_FOR_ANALYSIS
-
 #let's bring dates to a reasonable format
 #i decided to keep days,months and years in different columns, because
 #it may be convenient to select certain years/months/days as in the examples below
@@ -29,7 +27,6 @@ draftday<-data.table(newweather$day)
 day<- separate(draftday, V1, c("X", "day"),sep = 1)[,2]
 finalweather<-cbind(day, newweather[,-3])[,c(2,3,1,4:25)]
 finalweather1 <- finalweather %>% mutate_at(vars(-Events), as.numeric)
-
 View(finalweather1)
 
 #examples
@@ -60,4 +57,3 @@ hist(finalweather2$PrecipitationIn)
 
 saveRDS(finalweather1, "finalweather1.rds")
 saveRDS(finalweather2, "finalweather2NONA.rds")
-
