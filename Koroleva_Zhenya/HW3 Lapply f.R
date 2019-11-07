d4050746968e759d009bcdf23ea37d3c1faddcd3
Zhenya-Ko <56299927+Zhenya-Ko@users.noreply.github.com>
2019-11-07 23:33:20 +0300

    Update HW3 Lapply f.R

--- Koroleva_Zhenya/HW3 Lapply f.R
+++ Koroleva_Zhenya/HW3 Lapply f.R
@@ -1,10 +1,16 @@
 #first version only with numeric values
-ffordata1<-function(data, rowsel, colsel) {sum(unlist(lapply(subdata<- data[ rowsel, colsel],function(subdata)
-  +     +     { if (is.numeric(subdata) == TRUE) {matrix(subdata)}} )))}
+ffordata1<-function(data, rowsel, colsel) 
+  {sum(unlist(lapply(subdata<- data[ rowsel, colsel],
+                     function(subdata)
+    { if (is.numeric(subdata) == TRUE)
+    {matrix(subdata)}}
+    )))}
+
 #second try 
-ffordata1 <- function(data, rowsel, colsel) {unlist(lapply(subdata<- data[ rowsel, colsel],function(subdata)
-  + { if (is.numeric(subdata) == TRUE) {print(sum(matrix(subdata)))}
-    +     else if (subdata) {print(ftable(matrix(subdata)))}}
-  +  ))}
-# it checks only first two columns. I do not understand. Should I add more conditions?
-# No success with conditions... Should I create special function for that?
+ffordata1<- function(data, rowsel, colsel) 
+{lapply(subdata<- data[ rowsel, colsel],
+           function(subdata)
+           {ifelse (is.numeric(subdata), 
+                    sum(matrix(subdata)),
+                    list(ftable(subdata)))})
+}
