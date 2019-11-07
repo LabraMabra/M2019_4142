a0c6c9b68bcd7c6268d2375e2f9947a026a16bb6
ValeriaStavrova <55492085+ValeriaStavrova@users.noreply.github.com>
2019-11-07 10:52:16 +0300

    Update task3.R

--- Valeria_Stavrova/task3.R
+++ Valeria_Stavrova/task3.R
@@ -1,6 +1,6 @@
 #rewrite your function
-funfun1 <-function(data, rowset, colset)
-{append(list(data[rowset, colset]),lapply((data[rowset, colset]),
+funfun1 <-function(data, rowset, colset){
+append(list(data[rowset, colset]),lapply((data[rowset, colset]),
 function(x)ifelse(is.numeric(x), mean(x),list(table(x)))))}
 
 #treat numeric data as matrices
