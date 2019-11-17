348f1296e496ea4b57a15b72745cbefa5adc3be2
Polina Kochina <55478335+polika21@users.noreply.github.com>
2019-11-17 17:12:15 +0300

    Update task_02.R

--- Kochina_Polina/task_02.R
+++ Kochina_Polina/task_02.R
@@ -1,17 +1,18 @@
-my_func <- function(data, r_s, c_s){
-  subset <- data[r_s, c_s]
-  res <- list(subset)
-  
-  for (i in 1:length(subset)) {
-    if (is.numeric(subset[,i])){
-      summa <- sum(subset[,i])
-      res[[i+1]] <- summa
-    } else {
-      res[[i+1]] <- table(subset[,i])
-      
-    }
-  } 
-  res
+task_2_function <- function(data, r_s, c_s) {
+   subset <- data[r_s, c_s]
+   result <- list(subset)
+   
+   for(i in 1:length(subset)) {
+      if(is.numeric(subset[,i])){
+         summa <- sum(subset[,i])
+         result[[i+1]] <- summa
+      } else {
+         freq <- table(subset[,i])
+         result[[i+1]] <- freq
+      }
+   }
+   result
 }
 
-my_func(iris, c(1:150), c(1:5)) #example
+res_2 <- task_2_function(iris, 1:150, 1:5) #example
+res_2
