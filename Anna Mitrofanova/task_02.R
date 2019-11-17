cd116797c51aa5aa2ab7bb19cb170e0c4a447d2e
annani13 <43291984+annani13@users.noreply.github.com>
2019-11-17 23:23:08 +0300

    Update task_02.R

--- Anna Mitrofanova/task_02.R
+++ Anna Mitrofanova/task_02.R
@@ -1,38 +1,18 @@
-calculate_value <- function(column_data, column_name) {
-  new_value <- NULL
-  data_class <- class(column_data)
-  if (data_class == 'numeric') {
-    new_value <- sum(column_data)
-  } 
-  else if (data_class == 'character' || data_class == 'logical' || data_class == 'factor') 
-  {
-    new_value <- count(column_data, 1)
-  } 
-  new_value
-}
+iris <- read.csv("C:/Users/annaa/Downloads/iris.data", header=FALSE)
 
-main <- function(dataframe, row_selector, column_selector) {
-  new_dataframe <- dataframe[row_selector, column_selector]
-  new_values <- list()
-  for(column in names(new_dataframe)) {
-    new_values[[column]] <- calculate_value(new_dataframe[[column]], column)
+count <- function(data, row_s, col_s) {
+  subset <- data[row_s, col_s]
+  result <- list(subset)
+  
+  for(i in 1:length(subset)) {
+    if(is.numeric(subset[,i])){
+      result[[i]] <- sum(subset[,i])
+    } else {
+      freq <- table(subset[,i])
+      result[[i]] <- freq
+    }
   }
-  list(new_values, new_dataframe)
+ print(result) 
 }
 
-print_result  <- function(list_result) {
-  print(list_result[[1]])
-  print(paste0('columns: ', ncol(list_result[[2]]), ' rows: ',  nrow(list_result[[2]]) ) )
-}
-
-print('Indexes:')
-result <- main(example_dataframe, c(1,3,5,7,9,11))
-print_result(result)
-
-print('Logical vector:')
-result <- main(example_dataframe, column_selector=c(FALSE, TRUE, TRUE, TRUE, TRUE))
-print_result(result)
-
-print('Names:')
-result <- main(example_dataframe, column_selector=c('V1', 'V3'))
-print_result(result)
+result <- count(iris, 1:150, 1:5) 
