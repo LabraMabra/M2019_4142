69d9924fc61a752cac9d340970ade4563b2467d4
Baykov-Fedor <55537141+Baykov-Fedor@users.noreply.github.com>
2019-11-18 14:34:26 +0300

    Update task_04.R

--- Baykov_Fedor/task_04.R
+++ Baykov_Fedor/task_04.R
@@ -4,3 +4,5 @@ weather2 <- gather(weather1, key = day, value = val, '1' : '31')
 weather3 <- unite(weather2, col = y_m_d, year, month, day)
 weather4 <- spread(weather3, key = measure, value = val)
 weather5 <- weather4[order(weather4$y_m_d),]
+weather6 <- na.omit(weather5)
+
