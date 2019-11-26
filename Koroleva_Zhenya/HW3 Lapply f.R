93738a01f5fb8ccf0f05299885afe12b4a04d8ff
Zhenya-Ko <56299927+Zhenya-Ko@users.noreply.github.com>
2019-11-26 23:53:01 +0300

    Update HW3 Lapply f.R

--- Koroleva_Zhenya/HW3 Lapply f.R
+++ Koroleva_Zhenya/HW3 Lapply f.R
@@ -1,12 +1,3 @@
-#first version only with numeric values
-ffordata1<-function(data, rowsel, colsel) 
-  {sum(unlist(lapply(subdata<- data[ rowsel, colsel],
-                     function(subdata)
-    { if (is.numeric(subdata) == TRUE)
-    {matrix(subdata)}}
-    )))}
-
-#second try 
 ffordata1<- function(data, rowsel, colsel) 
 {lapply(subdata<- data[ rowsel, colsel],
            function(subdata)
