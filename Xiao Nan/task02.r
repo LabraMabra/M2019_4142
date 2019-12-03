12b92c92fcf17936e096960efd4891037e99b6f1
Xiao Nan <56390729+XiaoNan31@users.noreply.github.com>
2019-12-03 09:26:09 +0800

    Update task02.r

--- Xiao Nan/task02.r
+++ Xiao Nan/task02.r
@@ -1,5 +1,5 @@
-my_f <- function(data_frame, row_sel,col_sel) {
-  sub_data <- data_frame[row_sel,col_sel]
+my_f <- function(data_frame, row,col) {
+  sub_data <- data_frame[row,col]
   result_list <- list()
   for (i in colnames(sub_data)) {
     if (is.numeric(sub_data[,i])) {
@@ -16,4 +16,3 @@ my_f <- function(data_frame, row_sel,col_sel) {
 #for example
 my_f(iris, 26:125,c(1,2,5)) 
 my_f(iris, c(1,25,50,75,100,125,150), c(1:5))
-
