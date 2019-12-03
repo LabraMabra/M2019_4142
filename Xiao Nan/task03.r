e2ba0399fbaa3be02f45c91e0c55dc6f86a84119
Xiao Nan <56390729+XiaoNan31@users.noreply.github.com>
2019-12-03 23:29:01 +0800

    Update task03.r

--- Xiao Nan/task03.r
+++ Xiao Nan/task03.r
@@ -1,15 +1,17 @@
 in_f<-function(col){
   if (is.numeric(col) == TRUE) {
-    r_list<-mean(col)
+    return(mean(col))
   }
-  else {
-    r_list<summary(col)
+  else if (is.character(col) == TRUE ||
+           is.vector(col) == TRUE ||
+           is.factor(col) == TRUE) {
+    return(table(col))
   }
 }
 
 my_new_f<-function(data_frame,row,col){
   sub_data <- data_frame[row,col]
   for (i in colnames(sub_data)){
-    return(lapply(sub_data,in_f))
+    return(sapply(sub_data,in_f))
   }
 }
