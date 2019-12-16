ce995b199b15b2591297983a9bd0beca11b1b52c
ValeriaStavrova <55492085+ValeriaStavrova@users.noreply.github.com>
2019-12-16 11:31:34 +0300

    Update task5.R

--- Valeria_Stavrova/task5.R
+++ Valeria_Stavrova/task5.R
@@ -1,13 +1,18 @@
 library(dplyr)
 
 funfunf2 <- function(data,rowset,colset){
-  
   calc <- function(colset) {
     if (is.numeric(colset)) {
       result <- mean(colset)
     } else {
       result <- table(colset)
     }  }
-    
-  lapply((data[rowset, colset]), calc) %>% append(list(subset=data[rowset, colset]), 0)
+     lapply((data[rowset, colset]), calc) %>% append(list(subset=data[rowset, colset]), 0)
+}
+
+#another variant
+funfun3 <- function(data,rowset,colset){
+  lapply((data[rowset, colset]), 
+         function(x)ifelse(is.numeric(x), mean(x),list(table(x)))) %>% 
+  append(list(subset=data[rowset, colset]), 0)
 }
