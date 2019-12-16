2247d311c2b902872a4915268eebb06b2d557b4d
ValeriaStavrova <55492085+ValeriaStavrova@users.noreply.github.com>
2019-12-16 10:49:15 +0300

    Update task3.R

--- Valeria_Stavrova/task3.R
+++ Valeria_Stavrova/task3.R
@@ -1,11 +1,5 @@
 #rewrite your function
 funfun1 <-function(data, rowset, colset){
 append(list(data[rowset, colset]),lapply((data[rowset, colset]),
-function(x)ifelse(is.numeric(x), mean(x),list(table(x)))))}
-
-#treat numeric data as matrices
-library(dplyr)
-numfun <-function(data,rowset,colset){
- Filter(is.numeric,data[rowset,colset, drop = FALSE]) %>% colMeans %>% print}
-                                         
+function(x)ifelse(is.numeric(x), mean(x),list(table(x)))))}                                        
                                          
