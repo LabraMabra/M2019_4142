60dccf6abc8287e13648f03549302062e5872c34
anyaarz <33769083+anyaarz@users.noreply.github.com>
2019-12-24 18:05:58 +0300

    Create task_07.R

---
+++ Arzumanian_Anna/task_07.R
@@ -0,0 +1,92 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+data <-mtcars
+ui <-fluidPage(
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput(inputId = "title",
+                label="Plot title:",
+                placeholder = "Enter the title"),
+      selectInput(inputId = "x",
+                  label = "X-axis",
+                  choices = c("Miles/(US)gallon"="mpg", "Displacement"="disp", "Gross horsepower"="hp",
+                              "Rear axle ratio"="drat","Weight (1000 lbs)"= "wt","1/4 mile time"= "qsec"),
+                  selected= "mpg"),
+      selectInput(inputId = "y",
+                  label="Y-axis",
+                  choices = c("Miles/(US)gallon"="mpg", "Displacement"="disp", "Gross horsepower"="hp",
+                              "Rear axle ratio"="drat","Weight (1000 lbs)"= "wt","1/4 mile time"= "qsec"),
+                  selected= "disp"),
+      selectInput(inputId = "color",
+                  label="Color by",
+                  choices = c("Number of cylinders"="cyl","Engine type"="vs","Transmission"="am",
+                              "Number of forward gears"="gear","Number of carburetors"="carb"),
+                  selected= "Number of cylinders"),
+      sliderInput(inputId = "slider",
+                  label = "Alpha:",
+                  min=0, max=1,
+                  value=0.5),
+      numericInput(inputId = "size",
+                   label="Dot size:",
+                   min=1, max=9,
+                   value=5),
+      checkboxInput(inputId= "show_d",
+                    label="Show data",
+                    value=T),
+      checkboxInput(inputId= "show_s",
+                    label="Show summary",
+                    value=T),
+      submitButton("Apply Changes",
+                   icon = icon("check"))
+      
+    ),
+    
+    mainPanel(
+      plotOutput(outputId = "scatter"),
+      
+      fluidRow(
+        column(width = 4,
+               tableOutput(outputId = "data_selected")
+        ),
+        column(width=4,
+               tableOutput(outputId = "summary"))
+      )
+    )
+  )
+)
+
+server <-function(input,output){
+  
+  output$scatter <-renderPlot({
+    
+    req(input$size)
+    
+    ggplot(data, aes_string(x = input$x, y = input$y, color = input$color)) +
+      geom_point(alpha = input$slider, size = input$size) +
+      ggtitle(tools::toTitleCase(isolate(input$title)))
+    
+  })
+  
+  new_data <- reactive({
+    data %>% select_(input$x, input$y, input$color)
+  })
+  
+  output$data_selected <- renderTable({
+    if (input$show_d) {
+      new_data() 
+    }
+  })
+  
+  output$summary <- renderTable({
+    if (input$show_s) {
+      new_data() %>%
+        group_by_(input$color) %>% 
+        summarise_all(funs(mean, sd))
+    }
+  })
+}
+
+shinyApp(ui=ui, server=server)
