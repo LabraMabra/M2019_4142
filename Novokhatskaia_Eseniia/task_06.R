0a8cfbc12a5d8211ac1a376616759c46e440cdc9
EseniiaNovo <56360925+EseniiaNovo@users.noreply.github.com>
2019-12-24 18:16:17 +0200

    Add files via upload

--- Novokhatskaia_Eseniia/task_06.R
+++ Novokhatskaia_Eseniia/task_06.R
@@ -1,3 +1,5 @@
+library(gapminder)
+
 #ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) 
 #получилась одна диаграмма lifeExp и gdpPercap
 
@@ -11,4 +13,29 @@
 #исправлена шкала X для качественного распределения 
 
 #итоговая формула для графика: 
-ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point(aes(size = pop, color = continent)) + scale_x_log10() + facet_wrap(vars(year))
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(size = pop, color = continent)) + scale_x_log10() + 
+  facet_wrap(vars(year))
+
+#исправленные заголовки:
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + 
+  geom_point(aes(size = pop, color = continent)) + scale_x_log10() + 
+  facet_wrap(vars(year)) + 
+  labs(x = "GDP per capita", y = "Life expectancy", 
+       title = "The dependency of Life expectancy from GDP", 
+       subtitle = "The data is shown depending on different ages, continents and amount of population in particular country", 
+       size = "Population", color = "Continents")
+
+#другой вариант графика:
+ggplot(gapminder, aes(x = gdpPercap, y = pop)) + 
+  geom_point(aes(color = as.factor(year))) + scale_y_log10() + 
+  scale_x_log10() + facet_wrap(vars(continent)) + 
+  labs(x = "GDP per capita", y = "Population", title = "The dependency of Population from GDP", 
+       subtitle = "The data is shown depending on different years and continents of particular country", 
+       color = "Year")
+
+
+
+
+
+
